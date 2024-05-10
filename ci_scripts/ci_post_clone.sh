96aba00b0f05d89dea576f112b9585e28046f9b3
Yune Cho <fusma159@naver.com>
2024-05-10 22:25:04 +0900

    Modify ci_scripts

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -6,11 +6,11 @@
 #  Created by Yune Cho on 5/10/24.
 #
 
-FOLDER_PATH = "/Volumes/workspace/repository/FootballLog/Config.xcconfig"
+FOLDER_PATH = "/Volumes/workspace/repository/FootballLog"
 
 CONFIG_FILENAME = "Config.xcconfig"
 
-CONFIG_FILE_PATH = "$FOLDER_PATH / $CONFIG_FILENAME"
+CONFIG_FILE_PATH = "$FOLDER_PATH/$CONFIG_FILENAME"
 
 echo "APIKey = $APIKey" >> "$CONFIG_FILE_PATH"
 
